9977020f237f869b2f65dcf7cc4196177f483eba
Lawe Zangena <lawza02@gmail.com>
2021-09-10 15:54:25 +0200

    la till ignore

---
+++ CRM/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CRM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Person kund;
+            kund = new Person();
+
+            kund.ålder = "23";
+            kund.kön = "ickebinär";
+            kund.utbildning = "gymnasial";
+
+            Console.WriteLine($"Din kund är en {kund.ålder} årig { kund.kön} med en {kund.utbildning}. utbildning ");
+
+
+
+
+
+
+
+        }
+    }
+    class Person
+    {
+        public string ålder { get; set; }
+        public string kön { get; set; }
+        public string utbildning { get; set; }
+
+    }
+}
+    
