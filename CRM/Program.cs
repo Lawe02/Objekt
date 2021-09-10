9f12283916ceee8cc7968c3cd74044c495cae9c6
Lawe Zangena <lawza02@gmail.com>
2021-09-10 16:26:13 +0200

    klar med koden pls give helg

--- CRM/Program.cs
+++ CRM/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CRM
 {
@@ -6,20 +7,64 @@ namespace CRM
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Person> myPerson = new List<Person>();
+            while (true)
+            {
+                Console.WriteLine("1-Skapa ny kund, 2-Visa antal kunder, 3-Visa lista över alla kunder, 4-Avsluta");
+                string val = Console.ReadLine();
 
-            Person kund;
-            kund = new Person();
+                if (val == "1")
+                {
+                    Console.WriteLine("Ge ut kundens ålder");
+                    string kundÅlder = Console.ReadLine();
+                    Console.WriteLine("Ge ut kundens kön");
+                    string kundKön = Console.ReadLine();
+                    Console.WriteLine("Har kunden körkort?");
+                    string kundKort = Console.ReadLine();
 
-            kund.ålder = "23";
-            kund.kön = "ickebinär";
-            kund.utbildning = "gymnasial";
+                    myPerson.Add(new Person() { ålder = kundÅlder, kön = kundKön, körkort = kundKort });
 
-            Console.WriteLine($"Din kund är en {kund.ålder} årig { kund.kön} med en {kund.utbildning}. utbildning ");
+}
 
+                if (val == "2")
+                {
+                    Console.Clear();
+                    Console.WriteLine($" Det finns {myPerson.Count} personer i systemet .");
 
+                }
+                if (val == "3")  
+                {
+                    Console.Clear();
 
+                     
+                    foreach (Person c in myPerson)
+                    {
+                        Console.WriteLine($"Ålder: {c.ålder}\n" +
+                                          $"Kön: {c.kön}\n" +
+                                          $"Körkort: {c.körkort}\n");
+                    }
 
+                    if (val == "4")  
+                    {
+                        break;
+                         
+                    }
+                    if (val != "1" && val != "2" && val != "3" && val != "4")
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Du måste ange val 1, 2, 3, 4 för att få ut någonting.");
+                    }
+
+                     
+                    Console.ReadLine();
+
+                }
+            } 
+
+                
+            
+
+            
 
 
 
@@ -29,7 +74,7 @@ namespace CRM
     {
         public string ålder { get; set; }
         public string kön { get; set; }
-        public string utbildning { get; set; }
+        public string körkort { get; set; }
 
     }
 }
